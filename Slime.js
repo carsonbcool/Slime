7354d0f5b3e836fa86f0a6a5698a227f54f4aed9
carsonbcool <carsonbender777@gmail.com>
2026-03-15 18:52:38 -0400

    Update Slime.js

--- Slime.js
+++ Slime.js
@@ -1,8 +1,8 @@
-// This file handles the drawing and "blueprints" for the ball and slimes
+// This file handles the drawing of the ball and slimes
 function newBall(radius, color) {
   var img = new Image();
-  img.src = "vball.png"; // Must be lowercase on GitHub!
-
+  img.src = "vball.png"; // Ensure this is lowercase on GitHub!
+  
   return {
     radius: radius,
     color: color,
@@ -10,23 +10,27 @@ function newBall(radius, color) {
     y: 0,
     velocityX: 0,
     velocityY: 0,
+    rotation: 0,
     render: function() {
-      // Calculate position based on pixelsPerUnit from index.html
-      var xPix = this.x * pixelsPerUnit;
-      // courtY is roughly 80% of the canvas height
-      var courtY = gameHeight * 0.8; 
-      var yPix = courtY - (this.y * pixelsPerUnit);
-      var radiusPix = this.radius * pixelsPerUnit;
+      var xPix = this.x * pixelsPerUnitX;
+      var yPix = courtYPix - (this.y * pixelsPerUnitY);
+      var radiusPix = this.radius * pixelsPerUnitY + 2;
 
-      // Draw the ball image
-      ctx.drawImage(img, xPix - radiusPix, yPix - radiusPix, radiusPix * 2, radiusPix * 2);
+      // Add rotation like the original game
+      this.rotation += this.velocityX / 100;
+      
+      ctx.save();
+      ctx.translate(xPix, yPix);
+      ctx.rotate(this.rotation);
+      ctx.drawImage(img, -radiusPix, -radiusPix, radiusPix * 2, radiusPix * 2);
+      ctx.restore();
     }
   };
 }
 
 function newSlime(radius, color) {
   var img = new Image();
-  // If the color passed in is Red (#f00), use red image, otherwise green
+  // P1 is Red (#f00), P2 is Green (#0f0)
   img.src = (color === '#f00') ? "slime175red.png" : "slime175green.png";
 
   return {
@@ -37,14 +41,33 @@ function newSlime(radius, color) {
     velocityX: 0,
     velocityY: 0,
     render: function() {
-      var xPix = this.x * pixelsPerUnit;
-      var courtY = gameHeight * 0.8;
-      var yPix = courtY - (this.y * pixelsPerUnit);
-      var radiusPix = this.radius * pixelsPerUnit;
+      var xPix = this.x * pixelsPerUnitX;
+      var yPix = courtYPix - (this.y * pixelsPerUnitY);
+      var radiusPix = this.radius * pixelsPerUnitY;
 
-      // Draw the slime image (Slimes are half-circles, so height is radiusPix)
+      // Draw the slime image
       ctx.drawImage(img, xPix - radiusPix, yPix - radiusPix, radiusPix * 2, radiusPix);
+      
+      // Draw the Eye (the classic tournament look)
+      var onLeft = (this.color === '#f00'); 
+      var eyeX = this.x + (onLeft ? 1 : -1) * this.radius / 4;
+      var eyeY = this.y + this.radius / 2;
+      var eyeXPix = eyeX * pixelsPerUnitX;
+      var eyeYPix = courtYPix - (eyeY * pixelsPerUnitY);
+
+      ctx.fillStyle = '#fff';
+      ctx.beginPath();
+      ctx.arc(eyeXPix, eyeYPix, radiusPix / 4, 0, Math.PI * 2);
+      ctx.fill();
+
+      // Pupil follows the ball
+      var dx = ball.x - eyeX;
+      var dy = eyeY - ball.y;
+      var dist = Math.sqrt(dx * dx + dy * dy);
+      ctx.fillStyle = '#000';
+      ctx.beginPath();
+      ctx.arc(eyeXPix + (radiusPix/8 * dx/dist), eyeYPix + (radiusPix/8 * dy/dist), radiusPix / 8, 0, Math.PI * 2);
+      ctx.fill();
     }
   };
 }
-
